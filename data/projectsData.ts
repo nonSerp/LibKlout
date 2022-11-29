0f64e23f28cc6909d327d5a2be2d9397831495e6
nonSerp <fredfarwenel2010@gmail.com>
2022-11-29 07:52:05 +0000

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,18 @@
+const projectsData = [
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Android TV Remote',
+    description: `When it comes to the virtual world, everything is played online. Hence, this also means that there are numerous ways in which you can interact with them.`,
+    imgSrc: '/static/images/remote.webp',
+    href: '/blog/TV-Remote',
+  },
+]
+
+export default projectsData
